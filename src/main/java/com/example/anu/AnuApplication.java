b973d4d0b176ffbb5fa7a7ca185dfeebf6af9b7c
unknown <sathisusmitha@gmail.com>
2019-06-21 14:40:19 +0530

    first commit

---
+++ src/main/java/com/example/anu/AnuApplication.java
@@ -0,0 +1,13 @@
+package com.example.anu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnuApplication.class, args);
+	}
+
+}
